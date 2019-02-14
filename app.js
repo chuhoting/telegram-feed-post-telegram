18ce175b0e33f5574cad2ff6bd4ea4bd301395c5
chuhoting <chu-ho-ting@outlook.com>
2019-02-14 14:21:40 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
-const TelegramBot = require('node-telegram-bot-api');
-const ogs = require('open-graph-scraper');
-const firebase = require('firebase');
 const FeedSub = require('feedsub');
-const Telegraf = require('telegraf');
 const request = require("request");
-
 const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
 const adapter = new FileSync('db.json');
@@ -16,9 +11,6 @@ db.defaults({ feed: [] }).write();
 const token = 'TELEGRAMBOTTOKEN';
 const bot = new TelegramBot(token, {polling: true});
 
-bot.on('message', (msg) => {
-  bot.sendMessage(msg.chat.id, 'Ill have the tuna. No crust.');
-});
 
 //run node app.js on terminal and try talking to bot > we'll see successful replies
 
